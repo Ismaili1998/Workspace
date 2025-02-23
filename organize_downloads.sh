913a9ce7259f7ac699d785b0ce616536c7e56ada
ouss123-gif <oussamanoureddine1949@gmail.com>
2025-02-23 14:07:10 +0000

    organisation des fichiers dans le dossier Telechargements

--- organize_downloads.sh
+++ organize_downloads.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 # Définition du dossier cible
-DOWNLOADS="$HOME/Téléchargements"
+DOWNLOADS="/home/ouss/Téléchargements"
+cd $DOWNLOADS
+folder_image="images"
+folder_archives="archives"
+folder_video="video"
+folder_documents="doccuments"
+
+
+liste_folders=( "$folder_archives"
+"$folder_image" 
+"$folder_video" 
+"$folder_documents" ) 
+for currend_forder in "${liste_folders[@]}" ; do 
+  echo "$currend_forder"
+   
+if [ -d $currend_forder ] ; then
+echo "dossier deja crrie" 
+else mkdir $currend_forder
+fi 
+done 
