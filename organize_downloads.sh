45c804c5714945e074ab9701ac2514f817bcc9dc
belhaj021 <ab065151@gmail.com>
2025-02-23 14:30:24 +0000

    creation des sous dossier

--- organize_downloads.sh
+++ organize_downloads.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 # Définition du dossier cible
-DOWNLOADS="$HOME/Téléchargements"
+DOWNLOADS="/home/amine/Téléchargements"
+cd $DOWNLOADS
+
+folder_images="images"
+folder_document="document"
+folder_videos="videos"
+folder_archives="archives"
+
+liste_folder=( "$folder_images"
+"$folder_document"
+"$folder_videos"
+"$folder_archives" ) 
+
+for  current_folder in "${liste_folder[@]}" ; do
+    echo "$current_folder"
+if [ -d $current_folder ] ; then
+        echo "dossier deja cree"
+    else mkdir $current_folder ;
+fi
+    done
