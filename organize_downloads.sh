6e64e51f92998e6eb4439b577e5c6217f3938849
Larbi <larbi.kou@gmail.com>
2025-02-23 18:14:39 +0000

    Ajout des dossiers de destination

--- organize_downloads.sh
+++ organize_downloads.sh
@@ -2,3 +2,17 @@
 
 # Définition du dossier cible
 DOWNLOADS="$HOME/Téléchargements"
+
+# Dossiers de destination
+DOCUMENTS="$DOWNLOADS/Documents"
+IMAGES="$DOWNLOADS/Images"
+VIDEOS="$DOWNLOADS/Vidéos"
+ARCHIVES="$DOWNLOADS/Archives"
+MUSIC="$DOWNLOADS/Musique"
+
+# Créer les dossiers de destination s'ils n'existent pas
+mkdir -p "$DOCUMENTS"
+mkdir -p "$IMAGES"
+mkdir -p "$VIDEOS"
+mkdir -p "$ARCHIVES"
+mkdir -p "$MUSIC"
