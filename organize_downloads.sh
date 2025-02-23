6ee0b2e325346a8060d6c89d7e75853c4927a3ed
Abdessamadmoumni <abdessamadmoumni@gmail.com>
2025-02-23 14:22:58 +0000

    creation des sous dossiers

--- organize_downloads.sh
+++ organize_downloads.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-
 # Définition du dossier cible
-DOWNLOADS="$HOME/Téléchargements"
+download="/home/abdessamad/Téléchargements"
+image="$download/image"
+video="$download/video"
+documents="$download/documents"
+archive="$download/archive"
+list=($image $video $documents $archive)
+for curent_folder in ${list[@]}; do
+if [ -d $curent_folder ]; then 
+echo $curent_folder "existe"
+else
+mkdir $curent_folder
+echo $curent_folder "created"
+fi
+done
