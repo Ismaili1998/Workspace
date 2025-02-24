71d97ea849e8f3b36d1733a8d14b082227441b40
Zankalouni <zankalouni.crypto@gmail.com>
2025-02-24 20:55:39 +0000

    Création des sous-dossiers (s'ils n'existent pas)

--- organize_downloads.sh
+++ organize_downloads.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# Définition du dossier cible
-DOWNLOADS="$HOME/Téléchargements"
+# Dossier cible
+DOWNLOADS="/home/ana/Téléchargements"
+
+# Sous-dossiers
+IMAGES="$DOWNLOADS/Images"
+ARCHIVES="$DOWNLOADS/Archives"
+DOCUMENTS="$DOWNLOADS/Documents"
+VIDEOS="$DOWNLOADS/Vidéos"
+
+# Créer les sous-dossiers (s'ils n'existent pas)
+mkdir -p "$IMAGES" "$ARCHIVES" "$DOCUMENTS" "$VIDEOS"
+echo "Création des sous-dossiers terminés"
