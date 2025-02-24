e17be4be6f537a3f4fd8703b150824a3040491cd
Zankalouni <zankalouni.crypto@gmail.com>
2025-02-24 21:05:18 +0000

    Migration des photos (, , )

--- organize_downloads.sh
+++ organize_downloads.sh
@@ -12,3 +12,17 @@ VIDEOS="$DOWNLOADS/Vidéos"
 # Créer les sous-dossiers (s'ils n'existent pas)
 mkdir -p "$IMAGES" "$ARCHIVES" "$DOCUMENTS" "$VIDEOS"
 echo "Création des sous-dossiers terminés"
+
+
+# Déplacer les fichiers
+for file in "$DOWNLOADS"/*; do
+  if [ -f "$file" ]; then
+    ext="${file##*.}"
+    name="${file##*/}"
+
+    if [[ "$ext" == "jpg" || "$ext" == "jpeg" || "$ext" == "png" || "$ext" == "gif" ]]; then
+      mv "$file" "$IMAGES/$name"
+      echo "Déplacé $name vers Images"
+    fi
+  fi
+done
