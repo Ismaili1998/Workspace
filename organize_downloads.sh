1ebe79bf2f9314061eda6a187b4b9bdd73e52104
Abdessamadmoumni <abdessamadmoumni@gmail.com>
2025-02-27 22:40:33 +0000

    Moving files *.zip/*.tar/*.rar to Archive folder

--- organize_downloads.sh
+++ organize_downloads.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Définition du dossier cible
+#Creating folders in DOWNLOAD if they don't exist
 download="/home/abdessamad/Téléchargements"
 image="$download/image"
 video="$download/video"
@@ -8,9 +8,39 @@ archive="$download/archive"
 list=($image $video $documents $archive)
 for curent_folder in ${list[@]}; do
 if [ -d $curent_folder ]; then 
-echo $curent_folder "existe"
+echo $curent_folder "Already existed"
 else
 mkdir $curent_folder
-echo $curent_folder "created"
+echo $curent_folder "created successfully"
 fi
+done
+#Transfer and organize files by type 
+list_image=($download/*.jpg $download/*.png $download/*.gif)
+list_video=($download/*.mp4 $download/*.flv $download/*.mov)
+list_archive=($download/*.zip $download/*.tar $download/*.rar)
+list_documents=($download/*.pdf $download/*.docx $download/*.txt)
+
+for file in ${list_archive[@]}; do
+    if [ -f "$file" ]; then
+       mv $file $archive
+       echo "Mouving $file to $download has been done successfully"
+    fi
+done
+#for file in ${list_documents[@]}; do
+    if [ -f "$file" ]; then
+    mv $file $documents
+    echo "Mouving $file to $download has been done successfully"    
+    fi
+done
+#for file in ${list_image[@]}; do
+    if [ -f "$file" ]; then
+    mv $file $image
+    echo "Mouving $file to $download has been done successfully"
+    fi
+done
+#for file in ${list_video[@]}; do
+    if [ -f "$file" ]; then
+    mv $file $video
+    echo "Mouving $file to $download has been done successfully"
+    fi
 done
